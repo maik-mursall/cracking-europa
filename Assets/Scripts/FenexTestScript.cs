b26c0388f4ccd9c059f334fb888f358675c92260
Fenex <31255152+thumpnail@users.noreply.github.com>
2021-06-21 16:02:00 +0200

    Created the FenexTestScript.cs
    dont ask why!

---
+++ Assets/Scripts/FenexTestScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenexTestScript : MonoBehaviour {
+    [SerializeField] private GameObject spawnable;
+    [SerializeField] private GameObject center;
+
+    [SerializeField] private float forwardAngle = 0;
+    [SerializeField] private float minDistance = 0;
+    [SerializeField] private float maxDistance = 0;
+    
+    //Setting
+    
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //applying settings
+        
+    }
+}
